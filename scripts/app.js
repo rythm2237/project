557a6cc6adae5417b28f0016be9de9af38a2b50e
rythm2237 <y.t.alashti@gmail.com>
2023-07-21 09:57:51 +0200

    context menu ended

--- scripts/app.js
+++ scripts/app.js
@@ -9,8 +9,34 @@ header_btn.addEventListener("click", function(){
 } else {
         taggleMenue.style.left = "-22rem";
         headerIcon.className = "fa fa-bars";
-        
-
-
     }
 })
+
+const contextMenu = document.getElementById("context-menu")
+
+function contextHandler(event){
+    event.preventDefault()
+    if (contextMenu.style.display === 'none'){
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+        contextMenu.style.display = "block"
+    } else {
+        contextMenu.style.left = event.pageX + 'px'
+        contextMenu.style.top = event.pageY + 'px'
+    }
+}
+
+function contextHiden(){
+    contextMenu.style.display = "none"
+}
+function contextHidenByEscape(event){
+ 
+    if (event.keyCode === 27){
+        contextMenu.style.display = "none"
+        
+    }
+}
+
+document.body.addEventListener("contextmenu", contextHandler)
+document.body.addEventListener("click", contextHiden)
+document.body.addEventListener("keydown", contextHidenByEscape)
