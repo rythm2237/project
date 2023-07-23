5decaa7816378111cfbe6e7399281a9eee7edf16
rythm2237 <y.t.alashti@gmail.com>
2023-07-23 08:10:49 +0200

    sticky navbar

--- scripts/app.js
+++ scripts/app.js
@@ -40,3 +40,31 @@ function contextHidenByEscape(event){
 document.body.addEventListener("contextmenu", contextHandler)
 document.body.addEventListener("click", contextHiden)
 document.body.addEventListener("keydown", contextHidenByEscape)
+
+let companyName = document.querySelector(".companyName")
+window.addEventListener("DOMContentLoaded", function (){
+    companyName.style.transition = "all 3s ease 1s;"
+})
+
+//loader//
+let loadElem = document.querySelector('.loader')
+window.addEventListener('load', function(){
+    loadElem.classList.add("hidden")
+}) 
+
+// sticky navbar
+let nav = document.querySelector(".header-top")
+let logo = document.querySelector('.nav-logo img')
+document.addEventListener('scroll', function(){
+    if (document.documentElement.scrollTop > 0){
+        nav.classList.add('sticky')
+        nav.style.margin = "0"
+        logo.style.width = "3rem"
+    } else {
+        nav.classList.remove('sticky')
+        logo.style.width = "5rem"
+        nav.style.margin = "1.5rem"
+
+
+    }
+})
